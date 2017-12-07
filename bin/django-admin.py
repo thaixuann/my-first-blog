5e1e3526060b58dc674e9318158539d3fee36d50
Thai <thai@localhost.com>
2017-12-07 14:33:40 -0800

    My Django post app, first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/opt/python2.7.2/thaing/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ lib/python2.7/types.py
@@ -0,0 +1,1 @@
+/usr/cisco/packages/python/python-2.7.10/lib/python2.7/types.py
